ef496f3cc9ea0a353095fdf6422495d9f93ff6b5
nifemimatuluko <113442922+nifemimatuluko@users.noreply.github.com>
2025-01-24 17:06:02 +0100

    Update usercontroller.js

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -507,55 +507,14 @@ const hash = crypto.createHmac('sha512', secret).update(JSON.stringify(req.body)
 
     require('dotenv').config();
     
-         const nodemailer = require('nodemailer')
-         const transporter=  nodemailer.createTransport({
-          pool: true,
-          host: "smtp.gmail.com",
-          port: 465,
-          secure: true, // use TLS
-          auth: {
-            user: process.env.gmailusername,
-            pass: process.env.gmailpassword,
-          },
-        });
-        const ShortUniqueid = require('short-unique-id');
-
-        const numIds = event.data.metadata.custom_fields[2] // Adjust this value to generate the desired number of IDs
         
-        const uid = new ShortUniqueid({ length: 10 }); // Create an instance with desired ID length
         
-        let uniqueIds = [];
-        for (let i = 0; i < numIds; i++) {
-          uniqueIds.push(uid.randomUUID()); // Generate and push IDs to the array
-        }
-  //  console.log(findprices);
-  let token =uniqueIds
-  console.log(token);
        
        
         
-        const now = new Date();
-        const info = await transporter.sendMail({
-          from: '"nifemimatuluko@gmail.com', // sender address
-          to: ` ${event.data.customer.email}`, // list of receivers
-          subject: `Hello ${event.data.customer.name} your payments was succesful thanks for trusting us`, // Subject line
-          text: "order in the bag", // plain text body
-          text: ` here's Your Token(s)${token} present it at the event it would be used to verify your identity`,
-          
-         
-        });
+      
         
-        console.log("Message sent: %s", info.messageId);
-    const checkoutmodel = require('../models/checkoutmodel')
-    await checkoutmodel.create({
-    // i.e name:event.customers or event.data
-     checkoutemail:`${event.data.customer.email}`,
-     name:`${event.data.metadata.custom_fields[0]}`,
-     token:token,
-     amount:`${event.data.metadata.custom_fields[1]}`,
-     quantity:`${event.data.metadata.custom_fields[2]}`,
-     success: true,
-    })
+     
     }    
     }
     res.sendStatus(200);
