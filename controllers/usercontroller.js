cb1076adf179aa2c291603a21068de33fa1a264e
nifemimatuluko <113442922+nifemimatuluko@users.noreply.github.com>
2025-01-24 22:57:38 +0100

    Update usercontroller.js

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -501,17 +501,18 @@ const hash = crypto.createHmac('sha512', secret).update(JSON.stringify(req.body)
     // Retrieve the request's body
     const event = req.body;
     // Do something with event
-         const userId=req.user.id
+         const userId=req.user
+        console.log(userId)
     if (event && event.event === 'transfer.success') {
     console.log('transfer api is working');
-         let sender_current_balance = await userModel.findById(userId);
+        //  let sender_current_balance = await userModel.findById(userId);
     
-        // Update the Sender's balance
-        const amountsTobedeductedFrombalance = event.data.amount
-        await userModel.updateOne(
-          { _id: userId },
-          { $set: { balance: sender_current_balance.balance - parseInt(amountsTobedeductedFrombalance) } },
-        );
+        // // Update the Sender's balance
+        // const amountsTobedeductedFrombalance = event.data.amount
+        // await userModel.updateOne(
+        //   { _id: userId },
+        //   { $set: { balance: sender_current_balance.balance - parseInt(amountsTobedeductedFrombalance) } },
+        // );
 
         
     // console.log(event.data.customer.email);
